90b084d942f383f0711817953b69950745b119b9
Roman Golubev <gromniki@gmail.com>
2019-04-17 20:40:28 +0400

    Адаптировал меню и блок promo

---
+++ source/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Открытие/закрытие основного меню
+var mainMenu = document.querySelector('.js-main-nav');
+var showMainMenu = document.querySelector('.js-toggle');
+
+mainMenu.classList.remove('main-nav--nojs');
+
+showMainMenu.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  if (mainMenu.classList.contains('main-nav--closed')) {
+    mainMenu.classList.add('main-nav--opened');
+    mainMenu.classList.remove('main-nav--closed');
+  } else {
+    mainMenu.classList.remove('main-nav--opened');
+    mainMenu.classList.add('main-nav--closed');
+  }
+});
